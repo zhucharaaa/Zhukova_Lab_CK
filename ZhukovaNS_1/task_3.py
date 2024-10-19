25f004e36f2626628a7ecdcb294d1430c8e46d7d
zhucharaaa <zhukovaaa3781@gmail.com>
2024-10-19 12:33:24 +0300

    Add files via upload

---
+++ ZhukovaNS_1/task_3.py
@@ -0,0 +1,11 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+all_players = len(list_players)
+
+center_list = all_players // 2
+
+
+first = list_players[:center_list]
+second = list_players[center_list:]
+
+print(first)
+print(second)
