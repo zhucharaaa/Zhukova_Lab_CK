fa778952c95ca7652802ff88e642e02e09a04f1c
zhucharaaa <zhukovaaa3781@gmail.com>
2024-11-02 17:50:18 +0300

    Update task_1.py

--- ZhukovaNS_2/task_1.py
+++ ZhukovaNS_2/task_1.py
@@ -6,12 +6,10 @@ increase = 0.05  # Ежемесячный рост цен
 number_of_months = 0
 budget = money_capital    # Оставшиеся деньги
 
-while budget >= spend:
+while budget >= spend - salary:
+    number_of_months += 1  # Первый месяц без долга
     budget = budget + salary  # Получили зп
     budget = budget - spend   # Потратились
-    number_of_months += 1     # Первый месяц без долга
-    if number_of_months == 1:
-        continue
-    spend *= (1 + increase) #Теперь месячные расходы увеличиваются
+    spend *= 1 + increase #Теперь месячные расходы увеличиваются
 
 print("Количество месяцев, которое можно протянуть без долгов:", number_of_months)
